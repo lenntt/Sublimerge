30c576dfc651484db5a0b7d9a0710dd5b04f0412
Borys Forytarz <borys.forytarz@gmail.com>
2012-09-28 20:09:18 +0200

    license update

--- Sublimerge.py
+++ Sublimerge.py
@@ -1,4 +1,4 @@
- # Copyright (c) 2011 Borys Forytarz <borys.forytarz@gmail.com>
+ # Copyright (c) 2012 Borys Forytarz <borys.forytarz@gmail.com>
  #
  # Permission is hereby granted, free of charge, to any person
  # obtaining a copy of this software and associated documentation files
